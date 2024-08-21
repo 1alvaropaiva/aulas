81f842899864936b5810b4699146b3577c7a6110
1alvaropaiva <alvaropaiva.dev@gmail.com>
2024-08-20 22:17:01 -0300

    aula1

---
+++ 1.cpp
@@ -0,0 +1,106 @@
+/*#include <stdio.h>
+
+int main (){
+	int num1, num2, soma, dif;
+	printf("Exercicio 1.\n");
+	printf("Digite o primeiro numero: \n");
+	scanf("%d", &num1);
+	printf("Digite o segundo numero: \n");
+	scanf("%d", &num2);
+	soma = num1 + num2;
+	dif = num1 - num2;
+	printf("A soma dos numeros e %d, a diferença deles e %d.\n", soma, dif);
+	
+	
+	
+	return 0;
+}*/
+
+//exercicio1
+
+/*#include <stdio.h>
+
+int main (){
+	float num1, num2, num3, num4, num5, soma, media;
+	printf("Exercicio 1.\n");
+	printf("Digite cinco numeros: \n");
+	scanf("%f %f %f %f %f", &num1, &num2, &num3, &num4, &num5);
+	media = (num1 + num2 + num3 + num4 + num5) /5;
+	printf("%2.f", media);
+		
+	return 0;
+}*/
+
+//exercicio2
+
+/*#include <stdio.h>
+
+int main (){
+	float a, b, valx;
+	printf("digite um valor para a e b\n");
+	scanf("%f %f", &a, &b);
+	valx = (a + b) / (a - b);
+	printf("%2.f", valx);
+	
+	return 0;
+}*/
+
+//exercicio3
+
+/*#include <stdio.h>
+
+int main (){
+	int num1, num2, num3, d, r, s;
+	printf("Digite 3 numeros positivos e inteiros: \n");
+	scanf("%d %d %d", &num1, &num2, &num3);
+	r = (num1 + num2) * (num1 + num2);
+	s = (num2 + num3) * (num2 + num3);
+	d = (r + s) /2;
+	printf("%d", d);
+			
+	return 0;
+}*/
+
+//exercicio4
+
+/*#include <stdio.h>
+
+int main(){
+	int raio, volume, area;
+	printf("digite o raio da esfera: \n");
+	scanf("%d", &raio);
+	volume = 4 / 3 * 3,14 * (raio * raio);
+	area = 4 * 3,14 * (raio * raio); 
+	printf("o volume da circunferencia: %d\n", volume);
+	printf("a area da circunferencia: %d\n", area);
+	return 0;
+}*/
+
+//exercicio5
+
+/*#include <stdio.h>
+
+int main(){
+	float saldo, reajuste, novosaldo;
+	printf("digite o saldo: \n");
+	scanf("%f", &saldo);
+	reajuste = saldo / 100 * 5;
+	printf("o reajuste foi de: %2.f\n", reajuste);
+	novosaldo = saldo + reajuste;
+	printf("o novo saldo e: %2.f\n", novosaldo);
+	
+	return 0;
+}*/
+
+//exercicio6
+
+#include <stdio.h>
+
+int main(){
+	
+	
+	
+	
+	return 0;
+}
+
