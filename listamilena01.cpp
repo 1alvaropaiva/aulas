e881c7b62bc506400160b4e32f34a5bb677be54c
1alvaropaiva <alvaropaiva.dev@gmail.com>
2024-08-26 10:12:44 -0300

    dia26

---
+++ listamilena01.cpp
@@ -0,0 +1,91 @@
+//lista ufrj
+
+
+/*exercicio 01
+
+#include <stdio.h>
+int main(){
+
+	float p, q, r;
+	int x, y;
+	
+	
+	r = 15 / 2; //r = 7
+	printf("%f\n", r);
+	
+	r = r + 1.3; //r = 8.3
+	printf("%f\n", r);
+	
+	x = 2099 / 100.0; //x = 20.999
+	printf("%d\n", x);
+	
+	y = x - 0.1; //y = 20.899
+	printf("%d\n", y);
+	
+	p = y % 10; //p = 9.000
+	printf("%f\n", p);
+	
+	q = p + 1; //q = 10
+	printf("%f\n", q);
+	
+	x = q - 1.1; //x = 8
+	printf("%d\n", x);
+	
+	y = x - 5; //y = 3
+	printf("%d\n", y);
+	
+	y = y+y * y; //y = 12
+	printf("%d\n", y);
+		
+	return 0;
+}*/
+
+/*exercicio 04
+
+#include <stdio.h>
+
+int main(){
+	
+	int idade, ano, mes, dia;
+	printf("Digite sua idade em dias\n");
+	scanf("%d", &idade);
+	ano = idade / 365;
+	idade = idade % 365;
+	mes = idade / 30;
+	dia = idade % 30;
+	
+	
+	printf("Voce tem %d anos, %d meses e %d dias.", ano, mes, dia); 
+	
+	return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
